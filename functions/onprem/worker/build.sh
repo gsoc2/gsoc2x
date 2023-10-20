7ec50db4da24e94edddc89ef9e7052934bb64a99
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-10-21 04:43:26 +0600

    Create build.sh

---
+++ functions/onprem/worker/build.sh
@@ -0,0 +1,20 @@
+NAME=gsoc2-worker
+VERSION=1.2.0
+
+echo "Running docker build with $NAME:$VERSION"
+#CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o worker.bin .
+docker build . -t gsoc2/gsoc2:$NAME -t gsoc2/gsoc2:$NAME_$VERSION -t docker.pkg.github.com/gsoc2/gsoc2/$NAME:$VERSION -t ghcr.io/gsoc2/$NAME:$VERSION -t ghcr.io/gsoc2/$NAME:nightly -t ghcr.io/gsoc2/$NAME:$VERSION -t ghcr.io/gsoc2/$NAME:nightly
+
+# Push both for now..
+#docker push gsoc2/$NAME:$VERSION 
+#docker push gsoc2/gsoc2:$NAME_$VERSION
+#docker push docker.pkg.github.com/gsoc2/gsoc2/$NAME:$VERSION
+#docker tag gsoc2/gsoc2:0.8.51 ghcr.io/gsoc2/gsoc2-worker:0.8.5
+#docker tag gsoc2/gsoc2:$NAME ghcr.io/gsoc2/gsoc2-worker:0.8.52
+docker push gsoc2/gsoc2:$NAME
+docker push ghcr.io/gsoc2/$NAME:$VERSION
+docker push ghcr.io/gsoc2/$NAME:nightly
+
+docker push gsoc2/gsoc2:$NAME
+docker push ghcr.io/gsoc2/$NAME:$VERSION
+docker push ghcr.io/gsoc2/$NAME:nightly
